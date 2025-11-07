edbc2748f90ad5b1d150b12cf47a8137d8cc3048
Pedro Eckel <45345769+pedroeckel@users.noreply.github.com>
2025-11-07 17:23:43 -0300

    Update Main.java

--- demo/src/main/java/br/uniopet/Main.java
+++ demo/src/main/java/br/uniopet/Main.java
@@ -2,10 +2,6 @@ package br.uniopet;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/workspace_db";
-        Properties props = new Properties();
-        props.setProperty("user", "admin");
-        props.setProperty("password", "admin");
-        Connection conn = DriverManager.getConnection(url, props);
+        
     }
 }
